dcffc33f4e595adb32c0d06e819972b05b9064a2
Eli MArshall <elimarshall@Elis-Mac-mini.local>
2017-01-26 00:09:59 -0500

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularPracticeApp
+ * @description
+ * # angularPracticeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularPracticeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+      $locationProvider.html5Mode(true);
+      $locationProvider.hashPrefix('!');
+
+  });
